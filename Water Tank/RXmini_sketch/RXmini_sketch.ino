3738e1118888e492b63107324f6eb5bcc8f6fd32
Jacob Burghart <jjburghart@gmail.com>
2025-07-25 21:43:46 -0500

    updated radio channels and power levels

--- Water Tank/RXmini_sketch/RXmini_sketch.ino
+++ Water Tank/RXmini_sketch/RXmini_sketch.ino
@@ -1,14 +1,15 @@
 #include <SPI.h>
 #include <RF24.h>
-
-RF24 radio(9, 10);  // CE, CSN
+RF24 radio(7, 10); // CE, CSN
+//RF24 radio(9, 10);  // CE, CSN
 const uint64_t address = 0xE8E8F0F0E1LL;  // Must match transmitter
 
 void setup() {
   Serial.begin(9600);        
   radio.begin();
+  radio.setChannel(115); 
   radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_LOW);  // Or HIGH/MAX if needed
+  radio.setPALevel(RF24_PA_HIGH);  // Or HIGH/MAX if needed
   radio.openReadingPipe(0, address);
   radio.startListening();
 }
@@ -19,4 +20,8 @@ void loop() {
     radio.read(&received, sizeof(received));
     Serial.println(received);
   }
+  // else {
+  //   Serial.println("no Radio");
+  //   delay(200);
+  // }
 }
